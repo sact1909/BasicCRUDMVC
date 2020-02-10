3c7f44c2093c9a679a608c1bb882d4d5b3a0dd69
Steven Checo <steven.checo.19@gmail.com>
2020-02-10 01:12:13 -0500

    first commit

---
+++ MyFirstMVC/Controllers/UserController.cs
@@ -0,0 +1,90 @@
+﻿using MyFirstMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly MyFirstDbEntities db = new MyFirstDbEntities();
+        // GET: User
+        public ActionResult Index()
+        {
+
+            var Usuarios = db.T_USER.ToList();
+            
+            return View(Usuarios);
+        }
+
+        public ActionResult Create() {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(T_USER Usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.T_USER.Add(Usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else {
+                return View();
+            }
+            
+        }
+
+        public ActionResult Details(int ID) {
+            var Usuario = db.T_USER.Where(w => w.CODIGO == ID).FirstOrDefault();
+            return View(Usuario);
+        }
+
+        
+        public ActionResult Edit(int ID)
+        {
+
+            var Usuario = db.T_USER.Where(w => w.CODIGO == ID).FirstOrDefault();
+            return View(Usuario);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(T_USER Usuario)
+        {
+
+            if (ModelState.IsValid) {
+
+                db.Entry(Usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+
+            }
+            
+            
+        }
+
+        public ActionResult Delete(int ID) {
+
+            var usuarioDelete = db.T_USER.Where(w => w.CODIGO == ID).FirstOrDefault();
+            db.T_USER.Remove(usuarioDelete);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        
+
+
+
+
+
+    }
+}
